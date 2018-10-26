ed61fb09bb1e3aea677b8594f77f9c2297687660
Ethanwy <ethanofgg@gmail.com>
2018-10-26 16:56:14 +0800

    eureka server

---
+++ src/main/java/com/ethanwy/blogeurekaserver/BlogEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.ethanwy.blogeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BlogEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogEurekaServerApplication.class, args);
+    }
+}
